2b7e3e1175270885318e1cb9113826ebdab4a03e
Masood Alam <masoodalam.hunzai@gmail.com>
2019-01-24 16:36:29 +0500

    added feature-restAPI using Promises

---
+++ feature-RestAPI/index.js
@@ -0,0 +1,52 @@
+// REST API usind Promises
+class searchGitRepo {
+
+    constructor(){
+        this.http = new XMLHttpRequest();
+    }
+    getData(url){
+        return new Promise((resolve,reject) => {    
+        this.http.open("GET",url,true);
+        this.http.onload = () =>{
+            if(this.http.status === 200){             
+                resolve(this.http.responseText);
+            }else{
+                reject(console.log("something went wrong"));
+            }
+        }       
+         this.http.send();   
+    });
+  }
+}
+
+  document.body.addEventListener('click',function(e){
+      if(e.target.value == "submit"){
+      let userInput = document.getElementById('name').value;  
+      if(userInput !=""){
+        e.preventDefault();
+        const obj = new searchGitRepo();
+        obj.getData(`https://api.github.com/users/${userInput}/repos`).then(response => {
+        let ul = document.querySelector('ul');
+        ul.style.display = "flex";
+        ul.style.flexDirection = "column";
+        ul.style.justifyContent = "space-between";
+        ul.style.listStyleType = "none";
+        let responseResult = JSON.parse(response);
+        let records = responseResult;
+        for (const key in records) {
+            let li = document.createElement('li');
+            let  text = document.createTextNode(`${key} = ${records[key].url} : ${records[key].name}`);
+            li.appendChild(text);
+            ul.appendChild(li);
+       }
+       
+      }).catch(error => 
+            console.log(error +"error in repo")
+            ); 
+    userInput.value = "" ;
+    }else{
+        console.log("Please enter username");
+        e.preventDefault();
+    }
+  }
+  });
